ee98bf2b05c74a1733122a33a6e5c02c14853518
Thomas FATTAL <thomas.fattal@gmail.com>
2012-09-30 16:34:05 +0200

    Add node(id, Predecessor, Successor

--- src/node1.erl
+++ src/node1.erl
@@ -1,2 +1,25 @@
 -module(node1).
+-export([node/3]).
 
+node(Id, Predecessor, Successor) ->
+    receive
+	{key, Qref, Peer} ->
+	    Peer ! {Qref, Id},
+	    node(Id, Predecessor, Successor);
+	{notify, New} ->
+	    Pred = notify(New, Id, Predecessor),
+	    node(Id, Pred, Successor);
+	{request, Peer} ->
+	    request(Peer, Predecessor),
+	    node(Id, Predecessor, Successor);
+	{status, Pred} ->
+	    Succ = stabilize(Pred, Id, Successor),
+	    node(Id, Predecessor, Succ);
+	state ->
+	    io:format('Id : ~w~n Predecessor : ~w~n Successor : ~w~n', [Id, Predecessor, Successor]),
+	    node(Id, Predecessor, Successor);
+	stop -> ok;
+	_ ->
+	    io:format('Strange message received'),
+	    node(Id, Predecessor, Successor)
+    end.
