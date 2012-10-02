307830f6481c6576bd4d4c8c75c813e97214922d
Thomas FATTAL <thomas.fattal@gmail.com>
2012-10-02 20:05:13 +0200

    Add function create, lookup, split and merge

--- src/storage.erl
+++ src/storage.erl
@@ -1,1 +1,28 @@
 -module(storage).
+
+create() ->
+    [].
+
+add(Key, Value, List) ->
+    {Key, Value} | List.
+
+lookup(Key, List) ->
+   case lists:keyfind(Key, 1, List) of
+       {Key, Value} ->	     
+	   Value;
+       false ->
+	   io:format("Key not found").
+
+split(LocalKey, Delim, Storage) ->
+    {Split1, Split2} = lists:foldl(fun({Key,Value},{AccSplit1,AccSplit2}) ->						     
+					   case key:between(Key, Delim, LocalKey) of
+					       true ->
+						   %% keep for local node
+						   {[{Key,Value}|AccSplit1],AccSplit2};
+					       false ->
+						   {AccSplit1,[{Key,Value}|AccSplit2]}
+					   end					       
+				   end, {[],[]}, Storage).
+
+merge(List1, List2) ->
+    lists:append(List1, List2).
